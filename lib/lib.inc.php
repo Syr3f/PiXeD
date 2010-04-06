99e2ac93aaabafb0c80eef83c10f1363b496ec8b
Serafim Junior Dos Santos Fagundes <sjsantos@spytronic.ca>
2010-04-06 15:57:18 -0400

    adjusted lib file, removed CHANGELOG

--- lib/lib.inc.php
+++ lib/lib.inc.php
@@ -28,7 +28,7 @@ if (!defined("SPnDX"))
 
 ##	XD Base Library	
 ##
-require_once(SPnDXDIFF.SPnDXDIFFSEP.LIBPATH."/XH2.inc.php");
+require_once(LIBPATH.SPnDXDIFF.SPnDXDIFFSEP."/XH2.inc.php");
 
 
 ?>
